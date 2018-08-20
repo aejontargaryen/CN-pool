feff2c53c00ee2dbe54a7111beedd69209e910d3
æjontargaryen <34797913+aejontargaryen@users.noreply.github.com>
2018-08-20 17:27:31 -0400

    added Chainxp links

--- website_example/config.js
+++ website_example/config.js
@@ -1,13 +1,13 @@
 var api = "http://poolhost:8117";
 
 var email = "support@poolhost.com";
-var telegram = "https://t.me/YourPool";
-var discord = "https://discordapp.com/invite/YourPool";
+var telegram = "https://t.me/DRGL_is_Dragonglass";
+var discord = "https://discord.gg/T9FDfbE";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "https://drgl.info/block/{id}";
-var transactionExplorer = "https://drgl.info/transaction/{id}";
+var blockchainExplorer = "http://drgl.info/?hash={id}#blockchain_block";
+var transactionExplorer = "http://drgl.info/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
